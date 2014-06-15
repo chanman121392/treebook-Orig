d24dca7ef7e73380cd36582f962f9e1943e1206f
Ryan S. Chan <chanman121392@gmail.com>
2014-06-14 21:58:50 -0400

    Hey, the first last send

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+         #attr_accessible :first_name, :last_name, :profile_name
+
+   validates :first_name, presence: true
+
+   validates :last_name, presence: true
+   
+   validates :profile_name, presence: true,
+                            uniqueness: true,
+                            format: {
+                              with: /\A[a-zA-Z0-9_-]+\Z/,
+                              message: 'Must be Formatted Correctly'
+                            }
+
+
+  has_many :statuses
+
+  def full_name
+  	first_name+ " " + last_name
+  end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcase_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcase_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'b73a3624f2b37793bc8f5454b47b4e811ee1e2a39d459f027c3c4d88260758858fb2d0fa559bf324ac61761d9c454b70e78dfe15420c05318af6fd9097c86506'
